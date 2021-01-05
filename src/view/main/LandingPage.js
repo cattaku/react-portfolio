8b84b3800b28bd5e93d1d9385766c10476553c25
cattaku <wish8436@gmail.com>
2021-01-05 15:31:44 +0900

    test

---
+++ src/view/main/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <div>
+            <h3 style={{color:'#000'}}>안녕하세요</h3>
+        </div>
+    )
+}
+
+export default LandingPage
